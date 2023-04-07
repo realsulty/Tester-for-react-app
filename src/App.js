affd256dab4695513e0e570ec27259b0a6e4f9fb
realsulty <Sultanobm1@gmail.com>
2023-04-07 15:13:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+// This is called the app component // 
+function App(){
+    return (
+         
+        <div className="container"> 
+            
+        <h1>My App</h1>
+        <p>New world order</p>
+        </div>
+
+
+        )
+}
+export default App 
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom' 
+import './index.css'
+import App from './App'
+//this is the main  //
+ReactDOM.render( //this strict mode to add extra checks and warnings for the app // 
+<React.StrictMode> 
+    <App />
+</React.StrictMode>
+
+
+, document.getElementById('root'))
